54487fe74147d79b2f837056ecabc94f1bc1854f
mohamedlaminediabate <107290136+MHD-Lamine@users.noreply.github.com>
2025-04-14 21:23:14 +0000

    Migration : augmentation de la longueur du champ password (hash sécurisé)

--- backend/app/models.py
+++ backend/app/models.py
@@ -13,7 +13,7 @@ class User(db.Model):
     # Champs de base pour l'utilisateur
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(256), nullable=False)    
     role = db.Column(db.String(50), default='patient')  # Ex : patient, admin
 
     # Relations vers les autres tables
